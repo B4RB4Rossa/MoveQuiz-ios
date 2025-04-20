07f539c28c82d7edf52bbec6ca7059854d306f34
B4RB4Rossa <mr.ilepekhin@mail.ru>
2025-04-20 19:14:30 +0300

    задание по 6 спринту выполнено

---
+++ MovieQuizTests/buttonTests.swift
@@ -0,0 +1,40 @@
+import XCTest
+
+@testable import MovieQuiz
+
+class ButtonTests: XCTestCase {
+    
+    func testYesButton() {
+        sleep(3)
+        
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["Yes"].tap()
+        sleep(3)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
+    }
+
+    func testNoButton() {
+        sleep(3)
+        
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["No"].tap()
+        sleep(3)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+
+        let indexLabel = app.staticTexts["Index"]
+       
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
+        XCTAssertEqual(indexLabel.label, "2/10")
+    }
+
+}
